0f86c70a992152a7a7d121af7e23d04bfc7ca4a2
Steven Lee <mr938363@gmail.com>
2022-04-23 16:46:04 +0900

    Feat: Add encrpyt password and id

--- devcamper_api/controllers/auth.js
+++ devcamper_api/controllers/auth.js
@@ -4,12 +4,32 @@ const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Register user
-// @route   GET /api/v11/auth/register
+// @route   POST /api/v11/auth/register
 // @access  Public
 exports.register = asyncHandler( async (req, res, next) => {
+    const { name, email, password, role } = req.body;
 
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
+    // Create token
+    const token = user.getSignedJwtToken();
 
     res.status(200).json({
-        success: true
+        success: true, 
+        token
     });
 });
+
+
+
+
+
+
+
+
